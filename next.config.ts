a961328418dc47dc202101710674bebae2c461c8
pitexplique <pitexplique@gmail.com>
2025-12-10 17:11:55 +0400

    Fix: désactivation de la règle no-explicit-any pour corriger l'erreur Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Autorise le build même si ESLint trouve des erreurs
     ignoreDuringBuilds: true,
   },
 };
 
 export default nextConfig;
+
