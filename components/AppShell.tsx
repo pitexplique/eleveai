dbc52b9696bc396ef3463e37362b2b2c07769dc9
pitexplique <pitexplique@gmail.com>
2025-12-29 17:36:32 +0400

    SEO nikel

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,23 +1,18 @@
 "use client";
 
 import { ReactNode } from "react";
-import { usePathname } from "next/navigation";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function AppShell({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
-
-  // On masque le header/footer UNIQUEMENT sur /auth/*
-  const hideChrome = pathname.startsWith("/auth");
-
   return (
     <>
-      {!hideChrome && <Header />}
+      <Header />
       {children}
-      {!hideChrome && <Footer />}
+      <Footer />
     </>
   );
 }
+
 
 
