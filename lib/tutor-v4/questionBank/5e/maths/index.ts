d2d39b1b899b5b792c60de26e744a91a01c50e8d
pitexplique <pitexplique@gmail.com>
2026-04-15 10:14:20 +0400

    ixl

---
+++ lib/tutor-v4/questionBank/5e/maths/index.ts
@@ -0,0 +1,8 @@
+// lib/tutor-v4/questionBank/5e/maths/index.ts
+
+import type { TutorBankItemV4 } from "@/lib/tutor-v4/types";
+import { nombresRelatifsBank } from "./nombres-relatifs.bank";
+
+export const maths5eQuestionBank: TutorBankItemV4[] = [
+  ...nombresRelatifsBank,
+];
