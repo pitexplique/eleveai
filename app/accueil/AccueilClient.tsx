bab5f5184118a9adc1ea6b7303a037ab3be03947
pitexplique <pitexplique@gmail.com>
2026-05-13 17:40:55 +0400

    cm2

--- app/accueil/AccueilClient.tsx
+++ app/accueil/AccueilClient.tsx
@@ -19,7 +19,9 @@ export default function AccueilPage() {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => setOffset(window.scrollY);
+    const handleScroll = () => {
+      setOffset(window.scrollY);
+    };
 
     const checkMobile = () => {
       setIsMobile(window.innerWidth < 768);
@@ -44,7 +46,6 @@ export default function AccueilPage() {
 
   return (
     <main className="relative min-h-[120vh] overflow-hidden">
-      
       {/* IMAGE DE FOND */}
       <div
         className="fixed bottom-0 left-0 right-0 top-[72px] -z-10"
@@ -65,7 +66,7 @@ export default function AccueilPage() {
           "
         />
 
-        {/* OVERLAY */}
+        {/* OVERLAY léger pour lisibilité */}
         <div className="absolute inset-0 bg-black/10 sm:bg-black/5" />
       </div>
 
@@ -77,15 +78,16 @@ export default function AccueilPage() {
         }}
       >
         {/* CARDS */}
-<div
-  className="
-    mt-[30vh]
-    grid grid-cols-2
-    gap-3 px-4
-    sm:mt-[50vh]
-    sm:flex sm:max-w-6xl sm:flex-wrap sm:justify-center sm:gap-6
-  "
->
+        <div
+          className="
+            mt-[58vh]
+            grid grid-cols-2
+            gap-3 px-4
+            sm:mt-[64vh]
+            sm:flex sm:max-w-6xl sm:flex-wrap sm:justify-center sm:gap-6
+            lg:mt-[66vh]
+          "
+        >
           {cards.map((card, index) => (
             <Link
               key={index}
@@ -93,13 +95,13 @@ export default function AccueilPage() {
               className="
                 group relative
                 h-[92px] w-[176px]
-                sm:h-[115px] sm:w-[220px]
                 overflow-hidden rounded-3xl
                 border border-white/40
                 shadow-xl transition-all duration-300
                 hover:-translate-y-2 hover:scale-[1.05]
                 hover:shadow-[0_0_30px_rgba(255,255,255,0.35)]
                 focus:outline-none focus:ring-4 focus:ring-white/70
+                sm:h-[115px] sm:w-[220px]
               "
             >
               <Image
